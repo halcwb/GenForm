82855e41e1f286c7bfea7e230d0675ffe1698a90
Casper Bollen <halcwb@gmail.com>
2018-03-17 10:17:32 +0100

    chore: adding product lib

--- clean.sh
+++ clean.sh
@@ -4,14 +4,25 @@ rm -r src/Informedica.GenUtils.Lib/obj/
 rm -r src/Informedica.GenUnits.Lib/bin/
 rm -r src/Informedica.GenUnits.Lib/obj/
 
+rm -r src/Informedica.GenProduct.Lib/bin/
+rm -r src/Informedica.GenProduct.Lib/obj/
+
 rm -r tests/Informedica.GenUtils.Tests/bin/
 rm -r tests/Informedica.GenUtils.Tests/obj/
 
 rm -r tests/Informedica.GenUnits.Tests/bin/
 rm -r tests/Informedica.GenUnits.Tests/obj/
 
+rm -r tests/Informedica.GenProduct.Tests/bin/
+rm -r tests/Informedica.GenProduct.Tests/obj/
+
 rm -r packages/
 rm -r paket-files/
 
 rm paket.lock
 rm .paket/Paket.Restore.targets
+
+mono .paket/paket.exe install
+
+dotnet restore
+dotnet build
---
+++ src/Informedica.GenProduct.Lib/Scripts/Script.fsx
@@ -0,0 +1,1 @@
+﻿#load "Script.fsx"
