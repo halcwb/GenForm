c5301f15c6066036ca318fce122127f25c6bb2bd
Casper Bollen <halcwb@gmail.com>
2018-04-26 12:16:16 +0200

    refactor: genunits lib

---
+++ src/Informedica.GenForm.Lib/MinMax.fs
@@ -0,0 +1,96 @@
+﻿namespace Informedica.GenForm.Lib
+
+
+module MinMax =
+
+
+    type MinMax<'a> = 
+        | None
+        | Min of 'a
+        | Max of 'a
+        | MinAndMax of ('a * 'a)
+
+
+    let none = None
+
+
+    let createMin = Min
+
+
+    let createMax = Max
+
+
+    let valid min max = min <= max
+
+    let createMinAndMaxDef def min max = 
+        if valid min max then (min, max) |> MinAndMax
+        else def
+
+
+    let createMinAndMax min max = createMinAndMaxDef none min max
+
+
+    let setMinCond cond min minmax =
+        match minmax with
+        | MinAndMax (m, max) ->
+            if cond min m then createMinAndMaxDef minmax min max else minmax
+        | Max max -> createMinAndMaxDef minmax min max
+        | None    -> createMin min
+        | Min m   -> if cond min m then createMin min else minmax
+
+
+    let setMaxCond cond max minmax =
+        match minmax with
+        | MinAndMax (min, m) ->
+            if cond max m then createMinAndMaxDef minmax min max else minmax    
+        | Min min -> createMinAndMaxDef minmax min max
+        | None    -> createMax max
+        | Max m   -> if cond max m then createMax max else minmax
+
+
+    let setMin min minmax = setMaxCond (fun _ _ -> true) min minmax
+
+
+    let setMax max minmax = setMaxCond (fun _ _ -> true) max minmax
+
+
+    let setSmallerMin min minmax = setMinCond (<) min minmax
+
+
+    let setLargerMax max minmax = setMaxCond (>) max minmax
+
+
+    let setLargerMin min minmax = setMinCond (>) min minmax
+
+
+    let setSmallerMax max minmax = setMaxCond (<) max minmax
+     
+
+    let foldCond cond mms =
+        let condMax m1 m2 = cond m2 m1
+        mms |> List.fold (fun acc mm ->
+            match mm with
+            | None    -> acc
+            | Min min -> setMinCond cond min acc
+            | Max max -> setMaxCond condMax max acc
+            | MinAndMax (min, max) ->
+                acc
+                |> setMinCond cond min
+                |> setMaxCond condMax max
+        ) none
+     
+
+    let foldMinimize mms = foldCond (>) mms
+     
+
+    let foldMaximize mms = foldCond (<) mms
+
+
+    let inRange n minmax =
+        match minmax with
+        | None    -> true
+        | Min min -> n >= min
+        | Max max -> n <= max
+        | MinAndMax (min, max) -> n >= min && n <= max
+
+
