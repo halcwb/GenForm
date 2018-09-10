6d8af700dc2ba9227758edfafa979b7504fb8259
halcwb <halcwb@gmail.com>
2018-09-10 19:12:36 +0200

    docs: some demo samples and documentation of Units.lib

--- src/Informedica.GenUnits.Lib/scripts/Scripts.fsx
+++ src/Informedica.GenUnits.Lib/scripts/Scripts.fsx
@@ -18,25 +18,69 @@ module Tests =
         u |> toString |> printfn "%s"
         f u
 
+    (* CREATE VALUE UNITS *)
+
     // Some basic units
     let mg400 = 400N |> create Units.Mass.milliGram
     let ml50  = 50N |> create Units.Volume.milliLiter
+    let ml5  = 5N |> create Units.Volume.milliLiter
     let l5 = 5N |> create Units.Volume.liter 
 
+    // The count group is a special unit group 
+    // with only one unit: times.
+    let times3 = 3N |> create Units.Count.times
+    // Division or mutliplication with times
+    // results in unchanged units:
+    // 3 times * 5 ml = 15 ml
+    times3 * ml5
+
+    (* GET BASE AND UNIT VALUES *)
+
     // Get the base value of the units
-    ml50 |> toBase
-    l5 |> toBase
-    // Normal comparison doesn't work
+    ml5 |> toBase       // 1/200N i.e. 5 ml = 1/200 = 0.005 l
+    l5   |> toBase      // 5N i.e. 5 l = 5 l
+
+    // Get the unit values
+    ml5 |> toUnit     // 500N i.e. 5 l = 5000 ml
+    l5   |> toUnit    // 5 i.e. 5 l = 5 l
+
+    (* USE OF COMPARISON OPERATORS *)
+
+    // Normal comparison operators don't work
     (ml50 > l5) // Returns true, but is false
-    // Use the specific comparison
+
+    // Use the specific comparison operators
     (ml50 >? l5) // Returns correct true
 
+
+    (* CALCULATION WITH VALUE UNITS *)
+
+    // All four basic arrythmic operations can be performed
+    
+    // - multiplication
+    times3 * times3 // = 9 times
+    // - division
+    times3 / times3 // = 1 times
+    // - addition
+    times3 + times3 // = 6 times
+    // - subtraction 
+    times3 - times3 // = 0 times
+
+    // Addition and subtraction can only be performed within same unitgroups
+    (ml50 + l5)    // = 5.05 l
+    (mg400 + ml50) // System.Exception: cannot add or subtract different units Mass (MilliGram 1N) Volume (MilliLiter 1N)
+    
+    // When two valueunits with the same unitgroup are divided you get a count group
+    l5 / ml50  // = 100N times
+
     // Calculation with units
-    ((mg400 + mg400)/ ml50)     // 16 mg[Mass]/ml[Volume]
-    >>* ((*) ml50)              // 800 mg[Mass] 
-    >>* (fun vu -> vu / ml50)   // 16 mg[Mass]/ml[Volume]
-    >>* ((*) ml50)              // 800 mg[Mass]
+    ((mg400 + mg400)/ ml50)     // (400 mg[Mass] + 400 mg[Mass]) / 50 ml[Volume] = 16 mg[Mass]/ml[Volume]
+    >>* ((*) ml50)              // 16 mg[Mass]/ml[Volume] * 50 ml[Volume] = 800 mg[Mass] 
+    >>* (fun vu -> vu / ml50)   // 800 mg[Mass] / 50 ml[Volume] = 16 mg[Mass]/ml[Volume]
+    >>* ((*) ml50)              // 16 mg[Mass]/ml[Volume] * 50 ml[Volume] = 800 mg[Mass]
     |> toString
+
+    (* VALUE UNITS CAN BE CONVERTED TO DIFFERENT UNITS WITHIN THE SAME UNIT GROUP *)
 
     // Unit conversion
     l5                            // 5 l[Volume]
@@ -51,6 +95,7 @@ module Tests =
     |> (fun (ValueUnit(_, u)) ->
         u |> Group.unitToGroup
     ) // GeneralGroup "dose", OpPer, TimeGroup -> i.e. Dose/Time
+
 
     // Calculate and get all valid units for conversion
     mg400 / ml50 / (1N |> create Units.Time.day) // 8 mg[Mass]/ml[Volume]/day[Time]
--- todo.md
+++ todo.md
@@ -1,6 +1,7 @@
 Features:
 - Create doserule per frequency unit
 - Return a result object from the service to document errors
+- Disallow conversion to different unit groups
 
 Problems:
 - Fix mapping of service dto to match new doserules
